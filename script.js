e1c6ed1c4005da933539d57256b4d2d9b2c13999
ykcaj-i <jliufend@gmail.com>
2025-02-26 17:28:06 -0500

    x

--- script.js
+++ script.js
@@ -113,3 +113,16 @@ function darkMode() {
   
 
 */ 
+
+function copyLink() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+}
