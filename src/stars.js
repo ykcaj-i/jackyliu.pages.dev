5eb91dba9e9261820e9e3413719724b24df90779
ykcaj-i <jliuarc@gmail.com>
2025-03-20 20:35:57 -0400

    x

--- src/stars.js
+++ src/stars.js
@@ -7,7 +7,7 @@ element = document.getElementById('stars0').style.color = "#4effff86";
 element = document.getElementById('stars0').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (200 - 20) + 20) + "px";
 element = document.getElementById('stars0').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1000 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1000 - 200) + 200) + "px";
 element = document.getElementById('stars0').style.left = `${randomPixel}`;
 
 
@@ -15,7 +15,7 @@ element = document.getElementById('stars1').style.color = "#4effff86";
 element = document.getElementById('stars1').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars1').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1000 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1000 - 200) + 200) + "px";
 element = document.getElementById('stars1').style.left = `${randomPixel}`;
 
 
@@ -23,7 +23,7 @@ element = document.getElementById('stars2').style.color = "#4effff86";
 element = document.getElementById('stars2').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars2').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1000 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1000 - 200) + 200) + "px";
 element = document.getElementById('stars2').style.left = `${randomPixel}`;
 
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
@@ -31,167 +31,169 @@ element = document.getElementById('stars3').style.color = "#4effff86";
 element = document.getElementById('stars3').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars3').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1000 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1000 - 200) + 200) + "px";
 element = document.getElementById('stars3').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars4').style.color = "#4effff86";
 element = document.getElementById('stars4').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars4').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1000 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars4').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars5').style.color = "#4effff86";
 element = document.getElementById('stars5').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars5').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+
 element = document.getElementById('stars5').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars6').style.color = "#4effff86";
 element = document.getElementById('stars6').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars6').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars6').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars7').style.color = "#4effff86";
 element = document.getElementById('stars7').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars7').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars7').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars8').style.color = "#4effff86";
 element = document.getElementById('stars8').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars8').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars8').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars9').style.color = "#4effff86";
 element = document.getElementById('stars9').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars9').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars9').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars10').style.color = "#4effff86";
 element = document.getElementById('stars10').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (200 - 20) + 20) + "px";
 element = document.getElementById('stars10').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars10').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars11').style.color = "#4effff86";
 element = document.getElementById('stars11').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (200 - 20) + 20) + "px";
 element = document.getElementById('stars11').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars11').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars12').style.color = "#4effff86";
 element = document.getElementById('stars12').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars12').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars12').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars13').style.color = "#4effff86";
 element = document.getElementById('stars13').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars13').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars13').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars14').style.color = "#4effff86";
 element = document.getElementById('stars14').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars14').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars14').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars15').style.color = "#4effff86";
 element = document.getElementById('stars15').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars15').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars15').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars16').style.color = "#4effff86";
 element = document.getElementById('stars16').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars16').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars16').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars17').style.color = "#4effff86";
 element = document.getElementById('stars17').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars17').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars17').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars18').style.color = "#4effff86";
 element = document.getElementById('stars18').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars18').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars18').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars19').style.color = "#4effff86";
 element = document.getElementById('stars19').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars19').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars19').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars20').style.color = "#4effff86";
 element = document.getElementById('stars20').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars20').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars20').style.left = `${randomPixel}`;
 
+/* 
 element = document.getElementById('stars21').style.color = "#4effff86";
 element = document.getElementById('stars21').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars21').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars21').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars22').style.color = "#4effff86";
 element = document.getElementById('stars22').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars22').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars22').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars23').style.color = "#4effff86";
 element = document.getElementById('stars23').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars23').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars23').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars24').style.color = "#4effff86";
 element = document.getElementById('stars24').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars24').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars24').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars25').style.color = "#4effff86";
 element = document.getElementById('stars25').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars25').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars25').style.left = `${randomPixel}`;
 
 element = document.getElementById('stars26').style.color = "#4effff86";
 element = document.getElementById('stars26').style.position = "absolute";
 randomPixel = Math.round(Math.random() * (470 - 20) + 20) + "px";
 element = document.getElementById('stars26').style.top = `${randomPixel}`;
-randomPixel = Math.round(Math.random() * (1900 - 20) + 20) + "px";
+randomPixel = Math.round(Math.random() * (1800 - 500) + 500) + "px";
 element = document.getElementById('stars26').style.left = `${randomPixel}`;
 
+*/
